c8e3593fe26dbd5e32ec77a82b39615579eebec2
Hoang Nghia <hoangnghianguyendac@gmail.com>
2022-09-15 19:28:21 +0700

    delete item in list

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import {useState} from "react";
-import logo from './logo.svg';
-import Nav from './view/Nav';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import Nav from "./view/Nav";
+import "./App.css";
 import Todo from "./view/Todo";
-
 
 function App() {
   //create state name and function to update state  name
   const [name, setName] = useState("Nghia");
   // create state address
   const [address, setAddress] = useState("");
-  
-const [todos, setTodos] = useState([
-  {id: 1, title: "watching video", type: "work"},
-  {id: 2, title: "reading book", type: "study"},
-]);
+
+  const [todos, setTodos] = useState([
+    { id: 1, title: "watching video", type: "work" },
+    { id: 2, title: "reading book", type: "study" },
+  ]);
 
   const updateName = (event) => {
     setAddress(event.target.value);
@@ -25,28 +24,48 @@ const [todos, setTodos] = useState([
       alert("Please enter address");
       return;
     }
-      
-    console.log('clicked'); 
-    let newtodo={id: Math.floor(Math.random()*1000), title: address, type: "study"};
-    let todosCopy= [...todos, newtodo];
+
+    console.log("clicked");
+    let newtodo = {
+      id: Math.floor(Math.random() * 1000),
+      title: address,
+      type: "study",
+    };
+    let todosCopy = [...todos, newtodo];
     setTodos(todosCopy);
     setAddress("");
-  }
+  };
+
+  const handleDelete = (id) => {  
+    let todosCopy = [...todos];
+    let index = todosCopy.findIndex((item) => item.id === id);
+    todosCopy.splice(index, 1);
+    setTodos(todosCopy);
+    console.log("deleted >>>", id);
+  };
 
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          hello world with react and {name} 
-        </p>
-        <Todo todos={todos} title={'All todos'}></Todo>
-        <hr/>
-        <Todo todos={todos.filter(item => item.type==='study') } title={'Todo type study'}></Todo>
-        <input type={"text"} value={address} onChange={(event)=>updateName(event)}></input>
-        <button onClick={()=> handleClick()}> click me </button>
-       
+        <p>hello world with react and {name}</p>
+        <Todo
+         todos={todos} title={"All todos"}
+         handleDelete={handleDelete}
+         ></Todo>
+        <hr />
+        <Todo
+          todos={todos.filter((item) => item.type === "study")}
+          title={"Todo type study"}
+          handleDelete={handleDelete}
+        ></Todo>
+        <input
+          type={"text"}
+          value={address}
+          onChange={(event) => updateName(event)}
+        ></input>
+        <button onClick={() => handleClick()}> click me </button>
       </header>
     </div>
   );
--- src/view/Todo.js
+++ src/view/Todo.js
@@ -1,11 +1,19 @@
 const Todo=(props)=>{
-    let todos= props.todos;
+    //let todos= props.todos;
+    const {todos, title, handleDelete}= props;
+    const handleClickDelete = (id) => {
+       console.log("delete >>>>", id);
+       handleDelete(id);
+    }
     return(
         <div className="todos-container">
-            <h2>{props.title}</h2>
-          {todos.map ((item, index)=>{ return <div key={item.id}> {index+1} { item.title}</div>}
+            <h4>{title}</h4>
+          {todos.map ((item, index)=>{
+             return <div key={item.id}> {index+1} { item.title} <span style={{color:'red', cursor:'pointer'}} 
+             onClick={()=>handleClickDelete(item.id)}>x</span></div>}
            
           )}
+          <hr></hr>
         </div>
 
     )
